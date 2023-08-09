011db82acda7b0153ee1a46d7f6cdc80c4a87284
anisul islam <anisulislam980340@gmail.com>
2023-08-09 09:50:56 +0600

    bfs completed

---
+++ cycle-detectation.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N=1e3+5;
+
+//adjacency list
+vector<int>adjList[N];
+
+//booleen array
+bool visited[N];
+
+//level array
+int level[N];
+
+//dfs implementation and cycle detection
+bool dfs(int u, int p=-1){
+    visited[u]=true;
+    bool cycleExist=false;
+    for(int v: adjList[u]){
+        if(v==p) continue;
+        if(visited[v]==true) return true;
+        cycleExist=cycleExist|dfs(v,u);
+    }
+    return true;
+
+}
+int main(){
+    int n,m;
+    cin>>n>>m;
+    for(int i=0; i<m; i++){
+        int u, v;
+        cin>>u>>v;
+        adjList[u].push_back(v);
+        adjList[v].push_back(u);
+        
+    }
+    bool isCycle=false;
+    for(int i=0; i<n; i++){
+        if(visited[i]) continue;
+        isCycle|=dfs(i);
+    }
+    if(isCycle){
+        cout<<"Cycle detected"<<endl;
+    }
+    else {
+        cout<<"No Cycle detected"<<endl;
+    }
+    return 0;
+}
